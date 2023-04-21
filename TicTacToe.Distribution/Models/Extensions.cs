7503844f30603a89328a5d8144ba08b81ca98b6c
Joseph Wee <joseph.wee.tze.yeong@gmail.com>
2023-04-21 19:36:44 +0800

    Migrating to .NET Core - Added UI changes for Machine Learning Prediction in Tic-Tac-Toe Game razor component.

---
+++ TicTacToe.Distribution/Models/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Models;
+
+namespace TicTacToe.Extensions
+{
+    public static class Extensions
+    {
+        public static string ToDisplay(this TicTacToeGameStatus status)
+        {
+            if (status == TicTacToeGameStatus.InProgress)
+                return "In Progress";
+            else if (status == TicTacToeGameStatus.Player1Wins)
+                return "Player 1 Wins";
+            else if (status == TicTacToeGameStatus.Player2Wins)
+                return "Player 2 Wins";
+            else if (status == TicTacToeGameStatus.Draw)
+                return "Draw";
+
+            return string.Empty;
+        }
+    }
+}
