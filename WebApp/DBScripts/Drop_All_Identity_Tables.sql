57bc795911ee3ef9e98133c50edc53fc1d7d52fe
JosephWee <joseph.wee.tze.yeong@gmail.com>
2023-04-23 22:29:02 +0800

    Code cleanup - Removed obsolete .NET Framework code - Removed .NET Framework projects TicTacToeLib, UnitTests and WebApp - Made minor UI changes in BlazorServerApp

--- WebApp/DBScripts/Drop_All_Identity_Tables.sql
+++
@@ -1,6 +0,0 @@
-﻿drop table dbo.AspNetUserClaims
-drop table dbo.AspNetUserLogins
-drop table dbo.AspNetUserRoles
-drop table dbo.AspNetRoles
-drop table dbo.AspNetUsers
-drop table dbo.__MigrationHistory
