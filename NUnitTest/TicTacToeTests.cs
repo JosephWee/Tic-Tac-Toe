2302c18a2e66256fcda6262e4a1dc2d089337861
JosephWee <joseph.wee.tze.yeong@gmail.com>
2023-04-24 22:57:25 +0800

    Refactored NUnitTests into ComputerPlayerV2 tests and ComputerPlayerV3 tests

--- NUnitTest/TicTacToeTests.cs
+++ NUnitTest/TicTacToeTests.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 using System.Text;
 using Azure;
 using TicTacToe.BusinessLogic;
+using TicTacToe.Extensions;
 
 namespace UnitTests
 {
     public class TicTacToeTests
     {
         private static Random random = new Random();
+        private static string _MLModel1Path = string.Empty;
 
         [SetUp]
         public void TestSetup()
@@ -24,21 +26,44 @@ namespace UnitTests
                 .DbContextConfig
                 .AddOrReplace("TicTacToeData", TicTacToeDataConnString);
 
+            _MLModel1Path = Path.Combine(msbuildDir, "MLModels", "MLModel1.zip");
+        }
+
+        [Test]
+        public void TestValidGamesV2()
+        {
             TicTacToe
                 .BusinessLogic
                 .ComputerPlayerConfig
                 .RegisterComputerPlayer<ComputerPlayerV2>();
+
+            TestValidGames("V2");
         }
 
         [Test]
-        public void TestValidGames()
+        public void TestValidGamesV3()
+        {
+            TicTacToe
+                .BusinessLogic
+                .ComputerPlayerConfig
+                .RegisterComputerPlayer<ComputerPlayerV3>(
+                    () =>
+                    {
+                        var computerPlayer = new ComputerPlayerV3(_MLModel1Path);
+                        return computerPlayer;
+                    });
+
+            TestValidGames("V3");
+        }
+
+        public void TestValidGames(string Version)
         {
             int gamesCount = 100;
-
+            var gameOutcomes = new Dictionary<TicTacToe.Models.TicTacToeGameStatus, int>();
             for (int g = 0; g < gamesCount; g++)
             {
                 var gameStatus = TicTacToe.Models.TicTacToeGameStatus.InProgress;
-                string InstanceId = $"UnitTest {g} @ {DateTime.UtcNow.ToString("o")}";
+                string InstanceId = $"UnitTest {Version} {g} @ {DateTime.UtcNow.ToString("o")}";
                 var request = new TicTacToe.Models.TicTacToeUpdateRequest()
                 {
                     InstanceId = InstanceId,
@@ -71,13 +96,46 @@ namespace UnitTests
                     if (response.ComputerMove.HasValue)
                         request.CellStates[response.ComputerMove.Value] = 2;
                 }
+
+                if (gameOutcomes.ContainsKey(gameStatus))
+                    gameOutcomes[gameStatus] += 1;
+                else
+                    gameOutcomes.Add(gameStatus,1);
             }
+
+            Assert.IsNotNull(gameOutcomes);
         }
 
         [Test]
-        public void TestInvalidGames()
+        public void TestInvalidGamesV2()
         {
-            string InstanceId = $"UnitTest @ {DateTime.UtcNow.ToString("o")}";
+            TicTacToe
+            .BusinessLogic
+            .ComputerPlayerConfig
+            .RegisterComputerPlayer<ComputerPlayerV2>();
+
+            TestInvalidGames("V2");
+        }
+
+        [Test]
+        public void TestInvalidGamesV3()
+        {
+            TicTacToe
+            .BusinessLogic
+            .ComputerPlayerConfig
+            .RegisterComputerPlayer<ComputerPlayerV3>(
+                () =>
+                {
+                    var computerPlayer = new ComputerPlayerV3(_MLModel1Path);
+                    return computerPlayer;
+                });
+
+            TestInvalidGames("V3");
+        }
+
+        public void TestInvalidGames(string Version)
+        {
+            string InstanceId = $"UnitTest {Version} @ {DateTime.UtcNow.ToString("o")}";
             var request = new TicTacToe.Models.TicTacToeUpdateRequest()
             {
                 InstanceId = InstanceId,
